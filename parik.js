62259adcdf32da917b7b70ebc85170ad28f0f40e
Parikshit sharma <54639971+19BCS4520@users.noreply.github.com>
2021-06-18 23:19:34 +0530

    Add files via upload

---
+++ parik.js
@@ -0,0 +1,74 @@
+console.log("Hello world");
+    alert("Parikshit");
+  //  Different ways to write JavaScript 
+    document.write("hii sir");
+    console.warn("this is warning");
+    console.error("this is error");
+    // 2 java script variables
+/*
+//3 mutiline comment
+
+var num1=34;
+var num2=45;
+console.log(num1+num2);*/
+// 4 datatype n java script
+//number
+var n1=34;
+var n2=45;
+//String
+var str1="Parikshit";
+var str2="guddu";
+//object
+var marks = {
+    parikshit: 99,
+    guddu: 99,
+    harry: 99.9
+}
+console.log(marks)
+console.log(n1)
+console.log(n2)
+console.log(str1)
+console.log(str2)
+//bolleans
+var a=true;
+var b=false;
+console.log(a,b)
+//undefine and null
+var u=undefined;
+console.log(u)
+var n=null;
+console.log(n)
+/*at higth level two type of datatype
+1. Primitive datatype:undifine,null,number,bollean,symbol
+2. Reference datatype : Arrays ans Objects
+*/
+//Array
+var a=[1,2,"guddu",,3,4,5]
+// 5 operators in java script
+//airthmatic operators
+var x=34;
+var y=56;
+console.log("the value of the x+y",x+y)
+console.log("the value of the x-y",x-y)
+console.log("the value of the x*y",x*y)
+console.log("the value of the x/y",x/y)
+//Assignment operators
+var c=x;
+c+=2;
+console.log("value of c= ",c)
+//comparision operator
+var e=34;
+var r=56;
+console.log(e==r)
+console.log(e>=r)
+console.log(e<=r)
+console.log(e>r)
+console.log(e<r)
+//logical operators
+console.log(true && true)
+console.log(true && false)
+console.log(false && false)
+//logical or
+console.log(true || true)
+console.log(true || false)
+console.log(false || false)
