056f4b5a13f7047acee9aefa7a0dcb54f35f912c
Parikshit sharma <54639971+19BCS4520@users.noreply.github.com>
2021-06-19 23:58:10 +0530

    Add files via upload

---
+++ ArrayStringMethod.js
@@ -0,0 +1,24 @@
+let arr=["parik","guddu",34,null,true];
+//array method
+console.log(arr.length)
+arr.pop();  //remove one from last
+console.log(arr)
+arr.shift(); //remove first element
+console.log(arr)
+//unshift use add at starting
+arr.unshift("Kheshu");
+console.log(arr)
+//String to array
+arr.toString();
+console.log(arr)
+arr.sort();
+console.log(arr)
+
+
+
+//String Method in Java Script
+let a="Parikshit is good boy"
+console.log(a.length)
+console.log(a.indexOf("good"))
+console.log(a.slice(0,4))
+console.log(a.replace("Parikshit","Gudda"))
